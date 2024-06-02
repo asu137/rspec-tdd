531ec7d080a6e91c9e669e537dd9d936f7c70374
asuto-arai <asuto.arai@goals.co.jp>
2024-06-02 16:25:29 +0900

    fix: module

--- Part1/Chapter2/Dollar.rb
+++ Part1/Chapter2/Dollar.rb
@@ -1,16 +1,14 @@
-module Acme
-  module Part1
-    module Chapter2
-      class Dollar
-        attr_accessor :amount
+module Part1
+  module Chapter2
+    class Dollar
+      attr_accessor :amount
 
-        def initialize(amount)
-          @amount = amount
-        end
+      def initialize(amount)
+        @amount = amount
+      end
 
-        def times(multiplier)
-          @amount *= multiplier
-        end
+      def times(multiplier)
+        @amount *= multiplier
       end
     end
   end
--- Part1/Chapter2/MoneyTest.rb
+++ Part1/Chapter2/MoneyTest.rb
@@ -1,16 +1,14 @@
 require 'rspec'
 require_relative 'Dollar'
 
-module Acme
-  module Part1
-    module Chapter2
-      RSpec.describe Dollar do
-        it 'multiplies amount correctly' do
-          five = Dollar.new(5)
-          five.times(2)
+module Part1
+  module Chapter2
+    RSpec.describe Dollar do
+      it 'multiplies amount correctly' do
+        five = Dollar.new(5)
+        five.times(2)
 
-          expect(five.amount).to eq(10)
-        end
+        expect(five.amount).to eq(10)
       end
     end
   end
