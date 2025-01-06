c68b398a850a2864faa7f5732c74b6bf1019bac4
asuto-arai <asutoarai137@gmail.com>
2025-01-06 22:53:11 +0900

    fix

--- Part1/Chapter3/Dollar.rb
+++ Part1/Chapter3/Dollar.rb
@@ -8,11 +8,11 @@ module Part1
       end
 
       def times(multiplier)
-        return Dollar.new(amount * multiplier)
+        Dollar.new(amount * multiplier)
       end
 
       def equals(object)
-        return amount == object.amount
+        amount == object.amount
       end
     end
   end
