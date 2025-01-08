980588b9c437ec2abeb9f9e038214437dd8c7dce
asuto-arai <asutoarai137@gmail.com>
2025-01-08 23:18:26 +0900

    fix Part1-Chapter8

--- Part1/Chapter8/MoneyTest.rb
+++ Part1/Chapter8/MoneyTest.rb
@@ -14,6 +14,9 @@ module Part1
       it 'Equality' do
         expect(Money.dollar(5)).to eq(Money.dollar(5))
         expect(Money.dollar(5)).not_to eq(Money.dollar(6))
+        expect(Money.franc(5)).to eq(Money.franc(5))
+        expect(Money.franc(5)).not_to eq(Money.franc(6))
+        expect(Money.franc(5)).not_to eq(Money.dollar(5))
       end
 
       it 'FrancMultiplication' do
